887a33f09c1e07c90639e116b590f18682584335
Your Name <you@example.com>
2021-12-18 17:17:08 -0500

    Basic readme, tweaks

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # modbus-cli
 
 This is a basic tool to query modbus elements.
+
+Modbus is a protocol designed for communicating between electronic devices designed in the 1970s that continues to be used today despite modern alternatives existing.
+
+Modbus can be found in a wide variety of devices, my particular use case is around solar power systems which often have an undocumented modbus interface designed to be used with the manufacturer's add-on devices, which are universally awful.
+
+This tool allows you to query a single address or address block at a time, and output the results in a few formats. You could use this within a script to collect many data points however it is more intended to be a tool for scanning around and discovering those data points to be collected by another more appropriate tool.
+
+# Installation
+
+If you have a Go compiler installed, simply
+`go get github.com/squash/modbus-cli`
+This will download, complile, and install to your GOROOT/bin directory.
+
+# Using modbus-cli
+
+Run modbus-cli with -h for a list of options.
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"flag"
 	"fmt"
 	"log"
@@ -21,31 +22,36 @@ type RoverData struct {
 }
 
 type config struct {
-	Port    string
-	Device  int
-	Baud    int
-	Retries int
-	Address string
-	Bytes   uint
+	Port     string
+	Device   uint
+	Baud     int
+	Retries  uint
+	Address  string
+	Count    uint
+	OutputAs string
 }
 
 func main() {
 	var c config
 	flag.StringVar(&c.Port, "port", "/dev/ttyUSB0", "Serial device (example: /dev/ttyUSB0")
-	flag.IntVar(&c.Device, "device", 1, "Modbus device id to query")
+	flag.UintVar(&c.Device, "device", 1, "Modbus device id to query")
 	flag.IntVar(&c.Baud, "baud", 9600, "Baud rate")
 	flag.StringVar(&c.Address, "address", "0x100", "Data address to query (hex prefixed with 0x, or decimal)")
-	flag.UintVar(&c.Bytes, "bytes", 1, "Number of 8 bit bytes for result")
-	flag.IntVar(&c.Retries, "retries", 1, "Retry query this many times")
+	flag.UintVar(&c.Count, "count", 1, "Number of 16 bit registers to read")
+	flag.UintVar(&c.Retries, "retries", 1, "Retry query this many times")
+	flag.StringVar(&c.OutputAs, "output-as", "go", "Format to print output values. Options: hex, decimal, go")
 	flag.Parse()
 
-	for x := 0; x < c.Retries; x++ {
+	if c.OutputAs != "decimal" && c.OutputAs != "hex" && c.OutputAs != "go" {
+		log.Fatal("Output format invalid")
+	}
+	for x := uint(0); x < c.Retries; x++ {
 		handler := modbus.NewRTUClientHandler(c.Port)
 		handler.BaudRate = c.Baud
 		handler.DataBits = 8
 		handler.Parity = "N"
 		handler.StopBits = 1
-		handler.SlaveId = 1
+		handler.SlaveId = byte(c.Device)
 		handler.Timeout = 5 * time.Second
 
 		err := handler.Connect()
@@ -54,23 +60,43 @@ func main() {
 		}
 		defer handler.Close()
 		client := modbus.NewClient(handler)
-		var a uint64
+		var a uint16
 		if strings.HasPrefix(c.Address, "0x") {
 			tmp := c.Address[2:]
-			a, err = strconv.ParseUint(tmp, 16, 64)
+			b, err := strconv.ParseUint(tmp, 16, 16)
 			if err != nil {
 				log.Fatal(err)
 			}
+			a=uint16(b)
 		} else {
-			a, err = strconv.ParseUint(c.Address, 16, 64)
+			b, err := strconv.ParseUint(c.Address, 10, 16)
 			if err != nil {
 				log.Fatal(err)
 			}
+			a=uint16(b)
 		}
-		results, err := client.ReadHoldingRegisters(uint16(a), uint16(c.Bytes))
+		results, err := client.ReadHoldingRegisters(a, uint16(c.Count))
 		if err != nil {
+			if err.Error()=="serial: timeout" { 
+				continue
+			}
 			log.Fatal(err)
 		}
-		fmt.Printf("%#v", results)
+		switch c.OutputAs {
+		case "go":
+			fmt.Printf("%#v ", results)
+		case "hex":
+			for x:=uint(0);x<c.Count;x++ {
+				tmp := binary.BigEndian.Uint16(results[x*2:])
+				fmt.Printf("0x%x ", tmp)
+			}
+		case "decimal":
+			for x:=uint(0);x<c.Count;x++ {
+				tmp := binary.BigEndian.Uint16(results[x*2:])
+				fmt.Printf("%d ", tmp)
+			}
+		}
+		fmt.Println()
+		return
 	}
 }
